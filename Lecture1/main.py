bedb6182905bbd16d4f3a2978778cdf5d2b3d574
AndreevAV <koriolanrus@gmail.com>
2023-11-30 17:09:25 +0300

    Add Lecture1

---
+++ Lecture1/main.py
@@ -0,0 +1,25 @@
+n = 5
+print(type(n))
+n = 'wefwef\'wefwef'
+print(n)
+n = 'wef"wefwef"wef'
+print(n)
+
+# print('Строчный комментарий')
+
+"""
+Многострочный комментарий
+"""
+
+a = 5
+b = 5.89
+c = 'Hello'
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a, b, c))
+
+print("Введите первое число:")
+a = input()
+
+b = input('Введите второе число:')
+
+print(a, ' + ', b, ' = ', a + b)
